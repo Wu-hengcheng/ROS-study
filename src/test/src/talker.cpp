0ebaa10141b7a91e2fefc1edd5e66dd2a3dad816
Wu-hengcheng <18392873962@163.com>
2020-12-28 18:23:00 +0800

    test

--- src/test/src/talker.cpp
+++ src/test/src/talker.cpp
@@ -6,7 +6,7 @@ int main(int argc , char **argv)
 {
     ros :: init(argc, argv, "talker");
     ros::NodeHandle n;
-    ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter",1000);
+    ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
     ros::Rate loop_rate(10);
     int count = 0;
     while(ros::ok())
